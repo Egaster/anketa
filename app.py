e6dc65c8003426e704079f8787f1d38f3b6f4bff
Darya <s02210522@gse.cs,msu.ru>
2024-03-25 19:44:41 +0300

    test register + log in + test BD

--- app.py
+++ app.py
@@ -1,34 +1,92 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
 app = Flask(__name__)
- 
+app.secret_key = ' key'
+
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'user'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'LOGIN'
+
+mysql = MySQL(app)
+
+
 @app.route('/')
 def home():
     return render_template('home.html')  # Главная
 
+
 @app.route('/info')
 def info():
-    return render_template('info.html') # Общая информация про судебное или внесудебное
-                                        
+    return render_template('info.html')  # Общая информация про судебное или внесудебное
+
 
 @app.route('/full_info')
 def full_info():
-    return render_template('full_info.html') # Полная инфа после заполнения анкеты и авторизации
+    return render_template('full_info.html')  # Полная инфа после заполнения анкеты и авторизации
+
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html') # Личный кабинет с прошлыми результатами анкеты
+    return render_template('profile.html')  # Личный кабинет с прошлыми результатами анкеты
+
 
 @app.route('/settings')
 def settings():
-    return render_template('settings.html') # Настройки
+    return render_template('settings.html')  # Настройки
 
-@app.route('/sign-in')
+
+@app.route('/sign-in', methods=['GET', 'POST'])
 def sign_in():
-    return render_template('sign_in.html') # Страница входа с вводом логина и пароля
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM form WHERE username = %s AND password = %s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully!'
+            return render_template('home.html', msg=msg)
+        else:
+            msg = 'Incorrect username/password!'
+    return render_template('sign_in.html', msg=msg)
 
-@app.route('/sign-up')
+
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template('sign_up.html')  # Страница регистрации
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM form WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO form VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+            return render_template('home.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('sign_up.html', msg=msg)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
