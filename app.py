039d3b50b4aedf33cb2ea91acd03de0682fa2334
Половцев Александр Валериевич <avpolovtsev@edu.hse.ru>
2024-03-26 18:52:32 +0300

    Настройка авторизации/входа/выхода

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
+import hashlib
 
 app = Flask(__name__)
 app.secret_key = ' key'
@@ -10,6 +11,7 @@ app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'user'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'LOGIN'
+
 
 mysql = MySQL(app)
 
@@ -46,16 +48,16 @@ def sign_in():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
-        password = request.form['password']
+        hashed_password = hashlib.sha256(request.form['password'].encode('utf-8')).hexdigest()
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM form WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM form WHERE username = %s AND password = %s', (username, hashed_password,))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
             # msg = 'Logged in successfully!'
-            return render_template('home.html', msg=msg)
+            return redirect(url_for('home'))
         else:
             msg = 'Неверный логин/пароль!'
     return render_template('sign_in.html', msg=msg)
@@ -66,7 +68,7 @@ def sign_up():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'phone' in request.form:
         username = request.form['username']
-        password = request.form['password']
+        hashed_password = hashlib.sha256(request.form['password'].encode('utf-8')).hexdigest()
         phone = request.form['phone']
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -75,20 +77,24 @@ def sign_up():
         if account:
             msg = 'Такой аккаунт уже существует'
         elif not re.match(r'(\+7|8)\d{10}', phone):
-            msg = ''
+            msg = 'Введите корректный номер телефона!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Логин может содержать только буквы и цифры!'
-        elif not username or not password or not phone:
+        elif not username or not hashed_password or not phone:
             msg = 'Поля должны быть заполнены!'
         else:
-            cursor.execute('INSERT INTO form VALUES (NULL, %s, %s, %s)', (username, password, phone,))
+            cursor.execute('INSERT INTO form (`username`, `password`, `phone`) VALUES (%s, %s, %s)', (username, hashed_password, phone,))
             mysql.connection.commit()
             msg = 'Регистрация прошла успешно!'
-            return render_template('home.html', msg=msg)
+            return redirect(url_for('home'))
     elif request.method == 'POST':
         msg = 'Поля должны быть заполнены!'
     return render_template('sign_up.html', msg=msg)
 
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop('loggedin', None)
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
