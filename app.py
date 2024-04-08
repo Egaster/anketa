3d1abb2fc3d3380229dc5db7575c35177ec47901
Половцев Александр Валериевич <avpolovtsev@edu.hse.ru>
2024-04-08 18:55:30 +0300

    результаты судебного

--- app.py
+++ app.py
@@ -55,9 +55,11 @@ def form():
     question_id = int(session['history'][-1])
     question = questions[question_id]
     if question_id < 0:
-        print(session['history'])
         user_id = session.get('id')  
-        res = results[int(session['history'][-2])]
+        if question_id == -2:
+            res = html_for_court(session['answers'])
+        else:
+            res = results[int(session['history'][-2])]
         cursor = mysql.connection.cursor()
         print(user_id)
         cursor.execute('SELECT session FROM sessions WHERE uid = %s', (user_id,))
@@ -74,6 +76,7 @@ def form():
         print(res)
         saved = get_saved_answers_from_database_form(session_num[0])
         print(saved)
+        
         return render_template('form.html', question=question, res=res, saved=saved)
     return render_template('form.html', question=question)
 
@@ -172,9 +175,8 @@ def profile():
     cursor.close() 
     if num_of_sessions != ():
         for i in range(1, num_of_sessions[0][0]):
-            if(get_saved_answers_from_database(i)!=()):
-                saved.append(get_saved_answers_from_database(i))
-        return render_template('profile.html', session_data=dict(zip([x for x in range(1,len(saved) + 1)], saved)))  # Личный кабинет с прошлыми результатами анкеты  # Личный кабинет с прошлыми результатами анкеты
+            saved.append(get_saved_answers_from_database(i))
+        return render_template('profile.html', session_data=dict(zip(saved, [x for x in range(1,num_of_sessions[0][0])])))  # Личный кабинет с прошлыми результатами анкеты  # Личный кабинет с прошлыми результатами анкеты
     return render_template('profile.html')
 
 def get_saved_answers_from_database(session_num):
@@ -262,7 +264,22 @@ def sample_declaration_out_of_court():
 def creditors():
     return send_from_directory('/home/user/bankruptcy/static/docs', 'creditors.docx', as_attachment=False)
 
+def html_for_court(answers):
+    html = ''
+    if answers['16'] == 'yes':
+        html += "<p>При этом, важно учитывать следующий момент: когда Вы узнали (или должны были узнать), что не можете заплатить всем кредиторам, у Вас есть 30 рабочих дней, чтобы подать заявление в суд. Если не успеете, то вам придётся заплатить штраф от 1000 до 3000 рублей (п. 5 <a href='https://www.consultant.ru/document/cons_doc_LAW_34661/cd9e7b3faed04ce5a1863ac280a28ee438df0280/?ysclid=lumge4m3yy484154578', target='_blank'>ст. 14.13 КоАП РФ</a>).</p>"
 
+def html_for_court(answers):
+    html = ''
+    if '17' in answers and answers['17'] == 'yes':
+        html+= "<p>Вы понимаете, что не сможете оплатить долги по всем обязательствам в срок, а это дает Вам возможность открыть процедуру судебного банкротства.</p><p><b>Неплатежеспособность</b> имеет место при соблюдении хотя бы 1 из этих пунктов:<ol class='with-parentheses><li>были прекращены расчеты с кредиторами, то есть Вы перестали исполнять денежные обязательства и (или)  уплачивать обязательные платежи, срок исполнения которых наступил;</li><li>более чем 10% совокупного размера имеющихся у Вас денежных обязательств и (или) обязанности по уплате обязательных платежей, срок исполнения которых наступил, не исполнены Вами в течение более чем 1 месяца со дня, когда такие обязательства и (или) обязанность должны быть исполнены;</li><li>размер Вашей задолженности превышает стоимость имущества, в том числе права требования;</li><li>у Вас есть постановление об окончании исполнительного производства в связи с отсутствием имущества, на которое может быть обращено взыскание.</li><ol>"
+    if '18' in answers and answers['18'] == 'no':
+        html += "При этом, важно учитывать следующий момент: если у Вас нет имущества, помимо, например, единственного жилья, то у Вас есть право подать в суд заявление о признании банкротом (<a href='https://www.consultant.ru/document/cons_doc_LAW_39331/c2c8c81ee8e4bd843286b08b10607f00ec6ae073/?ysclid=lumgijnp2z11735395' target='_blank'>ст. 213.4 ФЗ «О банкротстве»</a>)."
+    if '16' in answers and answers['16'] == 'yes':
+        html += "<p>При этом, важно учитывать следующий момент: когда Вы узнали (или должны были узнать), что не можете заплатить всем кредиторам, у Вас есть 30 рабочих дней, чтобы подать заявление в суд. Если не успеете, то вам придётся заплатить штраф от 1000 до 3000 рублей (п. 5 <a href='https://www.consultant.ru/document/cons_doc_LAW_34661/cd9e7b3faed04ce5a1863ac280a28ee438df0280/?ysclid=lumge4m3yy484154578' target='_blank'>ст. 14.13 КоАП РФ</a>).</p>"
+    if '10' in answers and answers['10'] == 'no':
+        html += "<p>Важно также понимать, что если у Вас нет доказательств того, что Вы не можете оплачивать задолженности вследствие непредвиденных обстоятельств (авария, болезнь, нахождение в больнице, инвалидность, появление нетрудоспособности и т.д.), то суд может не принять заявление или же отказать в признании Вас банкротом в ходе судебного процесса, так как сочтет Ваше поведение недобросовестным (например, когда лицо берет большое количество кредитов, зная заранее, что не будет в состоянии это выплатить). </p>"
+    
 if __name__ == "__main__":
     #app.run(debug=True)
     app.run(debug=False, port=80, host='0.0.0.0')
